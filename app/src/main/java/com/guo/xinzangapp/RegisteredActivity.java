40f96260bebaf023589cf4f93f06d25159756cac
jackGWY <guo_wei_yue1@126.com>
2018-11-03 22:48:28 +0800

    correct error

--- app/src/main/java/com/guo/xinzangapp/RegisteredActivity.java
+++ app/src/main/java/com/guo/xinzangapp/RegisteredActivity.java
@@ -100,8 +100,8 @@ public class RegisteredActivity extends AppCompatActivity {
                 String phone = registerPhone.getText().toString().trim();
                 String uname = registerName.getText().toString().trim();
                 String regpass=registerPassword.getText().toString().trim();
-                String url = MyURL.SERVER+"/heart/login/register?phone="+phone+"&uname="+
-                        uname+"&regpass="+regpass;
+                String url = "http://192.168.123.226:8080/heart/login/register?phone="+phone+"&uname="+uname+"&regpass="+regpass;
+                //url = "http://192.168.123.226:8080/heart/login/register?phone=111&uname=zfj&regpass=123456";
                 try {
                     String jsonString=new HttpRequestor().doGet(url);
                     JSONObject jsonObject = new JSONObject(jsonString);
