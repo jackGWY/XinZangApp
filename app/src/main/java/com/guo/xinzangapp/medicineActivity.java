afc483717c5f2e7b630e4f043b403bff611b4f04
jackGWY <guo_wei_yue1@126.com>
2018-11-22 09:36:35 +0800

    add medicine things

--- app/src/main/java/com/guo/xinzangapp/medicineActivity.java
+++ app/src/main/java/com/guo/xinzangapp/medicineActivity.java
@@ -9,17 +9,20 @@ import android.widget.TextView;
 
 import com.guo.xinzangapp.medicine.bMedicine1Activity;
 import com.guo.xinzangapp.medicine.bMedicine2Activity;
+import com.guo.xinzangapp.medicineArticle.medicineAticle1Activity;
 
 public class medicineActivity extends AppCompatActivity {
 
-    private TextView bMedicine1,bMedicine2;
+    private TextView bMedicine1,bMedicine2,artical1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicine);
+
         bMedicine1 = (TextView) findViewById(R.id.bMedicine1);
         bMedicine2 = (TextView) findViewById(R.id.bMedicine2);
 
+        artical1= (TextView)findViewById(R.id.artical_1);
         bMedicine1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,5 +36,25 @@ public class medicineActivity extends AppCompatActivity {
                 startActivity(new Intent(medicineActivity.this,bMedicine2Activity.class));
             }
         });
+        setListener();
+    }
+
+    private void setListener(){
+        OnClick onClick = new OnClick();
+        artical1.setOnClickListener(onClick);
+    }
+
+    private class OnClick implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            Intent intent=null;
+                switch(v.getId()){
+                    case R.id.artical_1:
+                        intent=new Intent(medicineActivity.this,medicineAticle1Activity.class);
+                        break;
+                }
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/guo/xinzangapp/medicineArticle/medicineAticle1Activity.java
@@ -0,0 +1,69 @@
+package com.guo.xinzangapp.medicineArticle;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.guo.xinzangapp.R;
+
+public class medicineAticle1Activity extends AppCompatActivity {
+    private WebView mWv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicine_aticle1);
+        mWv1=(WebView) findViewById(R.id.wv1);
+        mWv1.getSettings().setJavaScriptEnabled(true);//设置支持javascript
+        mWv1.setWebViewClient(new MyWebViewClient());
+        mWv1.loadUrl("https://mp.weixin.qq.com/s/6-oVK4SIecYfuGW7sEdsAg");//公司的移动站点都是m.
+        mWv1.setWebChromeClient(new MyWebChromeClient());
+    }
+
+    class MyWebViewClient extends WebViewClient {//浏览器在自己的app上运行搜索
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            Log.d("WebView","onPageStarted...");
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            Log.d("WebView","onPageFinished...");
+            //mWvMain.loadUrl("javascript:alert('hello')");
+            // mWvMain.evaluateJavascript("javascript:alert('hello')",null);
+        }
+    }
+    class MyWebChromeClient extends WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {//进度条
+            super.onProgressChanged(view, newProgress);
+        }
+
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
+            setTitle(title);
+        }
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {//返回到自己的浏览器，而不是推到view
+        if(keyCode==KeyEvent.KEYCODE_BACK&&mWv1.canGoBack()){//如果返回键
+            mWv1.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
