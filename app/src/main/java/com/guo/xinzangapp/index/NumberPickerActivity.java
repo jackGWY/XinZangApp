1852bc17ef0c5c4b83a999de1b5d393fa0786a7f
jackGWY <guo_wei_yue1@126.com>
2019-04-10 21:56:36 +0800

    fbs空腹血糖

--- app/src/main/java/com/guo/xinzangapp/index/NumberPickerActivity.java
+++ app/src/main/java/com/guo/xinzangapp/index/NumberPickerActivity.java
@@ -49,6 +49,13 @@ public class NumberPickerActivity extends AppCompatActivity {
     private NumberPicker numberPicker_chol;
     private View chol_view;
     private int chol = 50;
+    //fbs 空腹血糖
+    private EditText et_fbs;
+    private Button submit_fbs;
+    private PopupWindow popupWindow_fbs;
+    private NumberPicker numberPicker_fbs;
+    private View fbs_view;
+    private int fbs = 80;
     //男女
     private RadioGroup mRg1;
     private String sex = "男";
@@ -103,9 +110,66 @@ public class NumberPickerActivity extends AppCompatActivity {
             }
         });
 
+        //fbs 空腹血糖 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        et_fbs = (EditText) findViewById(R.id.et_fbs);
+        et_fbs.setText(fbs + "mg/dl");
+        et_fbs.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                // 设置初始值
+                numberPicker_fbs.setValue(fbs);
+
+                // 强制隐藏键盘
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+                // 填充布局并设置弹出窗体的宽,高
+                popupWindow_fbs = new PopupWindow(fbs_view,
+                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                // 设置弹出窗体可点击
+                popupWindow_fbs.setFocusable(true);
+                // 设置弹出窗体动画效果
+                popupWindow_fbs.setAnimationStyle(R.style.AnimBottom);
+                // 触屏位置如果在选择框外面则销毁弹出框
+                popupWindow_fbs.setOutsideTouchable(true);
+                // 设置弹出窗体的背景
+                popupWindow_fbs.setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                popupWindow_fbs.showAtLocation(fbs_view,
+                        Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
+
+                // 设置背景透明度
+                WindowManager.LayoutParams lp = getWindow().getAttributes();
+                lp.alpha = 0.5f;
+                getWindow().setAttributes(lp);
+
+                // 添加窗口关闭事件
+                popupWindow_fbs.setOnDismissListener(new PopupWindow.OnDismissListener() {
+
+                    @Override
+                    public void onDismiss() {
+                        WindowManager.LayoutParams lp = getWindow().getAttributes();
+                        lp.alpha = 1f;
+                        getWindow().setAttributes(lp);
+                    }
+
+                });
+            }
+        });
+
+        //
+        submit_fbs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fbs = numberPicker_fbs.getValue();
+                System.out.println(fbs+"空腹血糖");
+                et_fbs.setText(fbs + "mg/dl");
+                popupWindow_fbs.dismiss();
+            }
+        });
         //血清胆固醇@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         et_chol = (EditText) findViewById(R.id.et_chol);
-        et_chol.setText(bloodPressure + "mmHg");
+        et_chol.setText(chol + "mg/dl");
         et_chol.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -156,7 +220,7 @@ public class NumberPickerActivity extends AppCompatActivity {
             public void onClick(View v) {
                 chol = numberPicker_chol.getValue();
                 System.out.println(chol+"胆固醇");
-                et_chol.setText(chol + "mmHg");
+                et_chol.setText(chol + "mg/dl");
                 popupWindow_chol.dismiss();
             }
         });
@@ -314,6 +378,16 @@ public class NumberPickerActivity extends AppCompatActivity {
         numberPicker_chol.setFocusableInTouchMode(false);
         numberPicker_chol.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
         setNumberPickerDividerColor(numberPicker_chol);
+        // fbs 空腹血糖
+        fbs_view = LayoutInflater.from(NumberPickerActivity.this).inflate(R.layout.popwindow_fbs, null);
+        submit_fbs = (Button) fbs_view.findViewById(R.id.submit_fbs);
+        numberPicker_fbs = (NumberPicker) fbs_view.findViewById(R.id.numberPicker_fbs);
+        numberPicker_fbs.setMaxValue(200);
+        numberPicker_fbs.setMinValue(0);
+        numberPicker_fbs.setFocusable(false);
+        numberPicker_fbs.setFocusableInTouchMode(false);
+        numberPicker_fbs.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+        setNumberPickerDividerColor(numberPicker_fbs);
     }
 
     /**
